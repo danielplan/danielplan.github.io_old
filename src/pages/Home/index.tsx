1b7e445b50de7da81ad1c0edbee05681271dad01
daniel plan <dplanoetscher@unibz.it>
2021-07-04 22:49:54 +0200

    Somewhat better landing page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,22 @@
 import './home.scss';
 import Page from 'components/layout/Page';
-import logo from 'images/Logo.png';
+import Logo from 'components/svg/Logo';
 
 export default function Home(): JSX.Element {
     return (
         <Page className="home-page">
-            <img src={logo} alt="Logo" />
-            This website will go online. Someday.
+            <section className="intro-section content-container">
+                <div className="inner">
+                    <Logo />
+                    <div className="text-overlay">
+                        <span className="text-uppercase subtitle">Hello I'm</span>
+                        <h1 className="heading xlarge text-primary">daniel plan</h1>
+                    </div>
+                </div>
+            </section>
+            <section className="content-container">
+                Here will be some content. Maybe... Someday?
+            </section>
         </Page>
     )
 }

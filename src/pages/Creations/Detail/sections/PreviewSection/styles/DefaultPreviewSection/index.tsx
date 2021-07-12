9b5bc31714a0fb8924f8fdfe31f792de0fcdb064
daniel plan <dani.plan@yahoo.de>
2021-07-12 22:18:33 +0200

    design changes

--- src/pages/Creations/Detail/sections/PreviewSection/styles/DefaultPreviewSection/index.tsx
+++ src/pages/Creations/Detail/sections/PreviewSection/styles/DefaultPreviewSection/index.tsx
@@ -2,7 +2,7 @@ import './default-preview-section.scss';
 import Image from 'components/ui/Image';
 
 export default function DefaultPreviewSection({ images }: { images: string[] }) {
-    return <section className="preview-section phone-mockups portfolio-items">
+    return <section className="preview-section default-previews portfolio-items">
         <div className="content-container">
 
             <div className="row">

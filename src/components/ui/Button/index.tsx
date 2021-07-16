941bd2f5aab9df3f26c177dfaf6c0c9294f10052
daniel plan <dani.plan@yahoo.de>
2021-07-16 21:38:33 +0200

    mobile navigation

--- src/components/ui/Button/index.tsx
+++ src/components/ui/Button/index.tsx
@@ -1,15 +1,16 @@
-import { ReactNode } from 'react';
+import { MouseEventHandler, ReactNode } from 'react';
 import { Link } from 'react-router-dom';
 import './button.scss';
 
 interface Props {
     route?: string;
-    children: ReactNode
+    children: ReactNode;
+    onClick?: MouseEventHandler;
 }
 
-export default function Button({ children, route }: Props): JSX.Element {
+export default function Button({ children, route, onClick }: Props): JSX.Element {
     if (route) {
-        return <Link to={route} className="button">{children}</Link>
+        return <Link to={route} className="button" onClick={onClick}>{children}</Link>
     }
-    return <button className="button">{children}</button>
+    return <button className="button" onClick={onClick}>{children}</button>
 }

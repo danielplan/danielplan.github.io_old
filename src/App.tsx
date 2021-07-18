7be33b744ee5fa6b5bba41ed2d00750701348d5f
daniel plan <dani.plan@yahoo.de>
2021-07-18 18:59:09 +0200

    router bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
 import Header from 'components/layout/Header';
 import Footer from 'components/layout/Footer';
